358804fa4864fb4ea2ccf968799b1de1801d92e7
SaurabPoudel <poudelsaurab20@gmail.com>
2024-07-10 12:46:17 +0545

    crawl single page with errror handling

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("Hello");
+const { crawlPage } = require("./crawl");
+
+function main() {
+  if (process.argv.length < 3) {
+    console.log("No website provided!!!, \nUsage: npm start website");
+    process.exit(1);
+  }
+  if (process.argv.length > 3) {
+    console.log("Too many command line args!!, \nUsage: npm start website");
+    process.exit(1);
+  }
+  for (const arg of process.argv) {
+    console.log(arg);
+  }
+  const baseURL = process.argv[2];
+  console.log(`starting crawl of ${baseURL} ...........`);
+  crawlPage(baseURL);
+}
+
+main();
